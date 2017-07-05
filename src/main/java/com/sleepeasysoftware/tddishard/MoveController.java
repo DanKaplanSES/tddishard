221c661d8ed6a9e10b0b5f80db97767a8834f4f1
Daniel Kaplan <tieTYT@gmail.com>
2017-07-05 16:19:50 -0700

    Make it pass

--- src/main/java/com/sleepeasysoftware/tddishard/MoveController.java
+++ src/main/java/com/sleepeasysoftware/tddishard/MoveController.java
@@ -4,7 +4,20 @@ import java.util.Map;
 
 class MoveController {
 
+    private BoardRepository boardRepository;
+    private PlayerMove playerMove;
+    private ServerMove serverMove;
+
+    public MoveController(BoardRepository boardRepository, PlayerMove playerMove, ServerMove serverMove) {
+        this.boardRepository = boardRepository;
+        this.playerMove = playerMove;
+        this.serverMove = serverMove;
+    }
+
     Map<String, String> move(long boardId, String sourcePosition, String destinationPosition) {
-        return null;
+        Map<String, String> boardBeforePlayerMove = boardRepository.findById(boardId);
+        Map<String, String> boardAfterPlayerMove = playerMove.execute(boardBeforePlayerMove, sourcePosition, destinationPosition);
+        Map<String, String> boardAfterServerMove = serverMove.execute(boardAfterPlayerMove);
+        return boardRepository.save(boardAfterServerMove);
     }
 }
