b2b9c9ae8c3364367dd38f0891b7699443d13884
Daniel Kaplan <tieTYT@gmail.com>
2017-07-05 16:27:43 -0700

    Passing test

--- src/main/java/com/sleepeasysoftware/tddishard/MovePiece.java
+++ src/main/java/com/sleepeasysoftware/tddishard/MovePiece.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 class MovePiece {
     Map<String, String> execute(Map<String, String> board, String source, String destination) {
-        return null;
+        String piece = board.remove(source);
+        board.put(destination, piece);
+        return board;
     }
 }
