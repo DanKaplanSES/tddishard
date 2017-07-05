722a64cc676e3745e2515566621aad0429468c24
Daniel Kaplan <tieTYT@gmail.com>
2017-07-05 16:17:11 -0700

    Thread through dependencies to get output

--- src/test/java/com/sleepeasysoftware/tddishard/MoveControllerCollaborationTest.java
+++ src/test/java/com/sleepeasysoftware/tddishard/MoveControllerCollaborationTest.java
@@ -18,11 +18,31 @@ public class MoveControllerCollaborationTest {
     @InjectMocks
     private MoveController subject;
 
+    //DATA
+    @Mock
+    private Map<String, String> boardBeforePlayerMove;
+    @Mock
+    private Map<String, String> boardAfterPlayerMove;
+    @Mock
+    private Map<String, String> boardAfterServerMove;
     @Mock
     private Map<String, String> savedBoard;
 
+    //DEPENDENCIES
+    @Mock
+    private BoardRepository boardRepository;
+    @Mock
+    private PlayerMove playerMove;
+    @Mock
+    private ServerMove serverMove;
+
     @Test
     public void callReturnsBoard() throws Exception {
+        when(boardRepository.findById(1L)).thenReturn(boardBeforePlayerMove);
+        when(playerMove.execute(boardBeforePlayerMove, "a2", "a3")).thenReturn(boardAfterPlayerMove);
+        when(serverMove.execute(boardAfterPlayerMove)).thenReturn(boardAfterServerMove);
+        when(boardRepository.save(boardAfterServerMove)).thenReturn(savedBoard);
+
         Map<String, String> response = subject.move(1L, "a2", "a3");
 
         assertThat(response, equalTo(savedBoard));
