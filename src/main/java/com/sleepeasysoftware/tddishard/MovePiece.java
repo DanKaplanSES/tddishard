0a7272a19ecfee6d281a4747d12af4cab242a95c
Daniel Kaplan <tieTYT@gmail.com>
2017-07-05 16:23:54 -0700

    Make it compile

---
+++ src/main/java/com/sleepeasysoftware/tddishard/MovePiece.java
@@ -0,0 +1,10 @@
+package com.sleepeasysoftware.tddishard;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class MovePiece {
+    Map<String, String> execute(Map<String, String> board, String source, String destination) {
+        return null;
+    }
+}
---
+++ src/main/java/com/sleepeasysoftware/tddishard/ValidateMove.java
@@ -0,0 +1,9 @@
+package com.sleepeasysoftware.tddishard;
+
+import java.util.Map;
+
+class ValidateMove {
+    void execute(Map<String, String> board, String source, String destination) {
+
+    }
+}
