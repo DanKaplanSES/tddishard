25379f55668ff0b49bee1c929fc0beb2d0a5fc8d
Daniel Kaplan <tieTYT@gmail.com>
2017-07-05 16:15:59 -0700

    Make it compile

---
+++ src/main/java/com/sleepeasysoftware/tddishard/MoveController.java
@@ -0,0 +1,10 @@
+package com.sleepeasysoftware.tddishard;
+
+import java.util.Map;
+
+class MoveController {
+
+    Map<String, String> move(long boardId, String sourcePosition, String destinationPosition) {
+        return null;
+    }
+}
