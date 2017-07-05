7d933a98ac37c1a0f17ca5d3c5f1e26be0c7e6ac
Daniel Kaplan <tieTYT@gmail.com>
2017-07-05 16:26:54 -0700

    Write a failing logic test

---
+++ src/test/java/com/sleepeasysoftware/tddishard/MovePieceLogicTest.java
@@ -0,0 +1,32 @@
+package com.sleepeasysoftware.tddishard;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.*;
+
+public class MovePieceLogicTest {
+
+    private MovePiece movePiece;
+
+    @Before
+    public void setUp() throws Exception {
+        movePiece = new MovePiece();
+    }
+
+    @Test
+    public void outputHasPieceMoved() throws Exception {
+        Map<String, String> input = new HashMap<String, String>();
+        input.put("a2", "fooPiece");
+
+        Map<String, String> output = movePiece.execute(input, "a2", "a3");
+
+        assertThat(output.get("a2"), nullValue());
+        assertThat(output.get("a3"), equalTo("fooPiece"));
+    }
+}
