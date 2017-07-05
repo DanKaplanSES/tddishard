5ecf50158a59e54c647229fff1ff04d863b8b845
Daniel Kaplan <tieTYT@gmail.com>
2017-07-05 16:25:08 -0700

    Make it pass

--- src/main/java/com/sleepeasysoftware/tddishard/PlayerMove.java
+++ src/main/java/com/sleepeasysoftware/tddishard/PlayerMove.java
@@ -4,7 +4,16 @@ import java.util.Map;
 
 class PlayerMove {
 
+    private MovePiece movePiece;
+    private ValidateMove validateMove;
+
+    public PlayerMove(MovePiece movePiece, ValidateMove validateMove) {
+        this.movePiece = movePiece;
+        this.validateMove = validateMove;
+    }
+
     Map<String, String> execute(Map<String, String> board, String source, String destination) {
-        return null;
+        validateMove.execute(board, source, destination);
+        return movePiece.execute(board, source, destination);
     }
 }
